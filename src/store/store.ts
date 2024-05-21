bf8ec27f63d6b831328acbed41441cd9e2f59913
seunosinowo <oluwaseunpaul98@gmail.com>
2024-05-21 15:02:11 +0100

    Added Lists to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -14,9 +14,130 @@ export const useStore = create(
             FavoritesList: [],
             CartList: [],
             OrderHistoryList: [],
+            addToCart: (cartItem: any) =>
+                set(
+                    produce(state => {
+                        let found = false;
+                        for (let i = 0; i < state.CartList.length; i++) {
+                            if (state.CartList[i].id == cartItem.id) {
+                                found = true;
+                                let size = false;
+                                for (let j = 0; j < state.CartList[i].prices.length; j++) {
+                                    if (
+                                        state.CartList[i].prices[j].size == cartItem.prices[0].size
+                                    ){
+                                        size = true;
+                                        state.CartList[i].prices[j].quantity++;
+                                        break;
+                                    }
+                                }
+                                if (size == false) {
+                                    state.CartList[i].prices.push(cartItem.prices[0]);
+                                }
+                                state.CartList[i].price.sort((a: any, b: any) => {
+                                    if (a.size > b.size) {
+                                        return -1;
+                                    }
+                                    if (a.size < b.size) {
+                                        return 1;
+                                    }
+                                    return 0;
+                                });
+                                break;
+                            } 
+                        }
+                            if (found == false) {
+                                state.CartList.push(cartItem);
+                            }   
+                    }),
+                ),
+
+            calculateCartPrice: () =>
+                set(
+                    produce(state => {
+                        let totalprice = 0;
+                        for (let i = 0; i < state.CartList.length; i++) {
+                            let tempprice = 0;
+                            for (let j = 0; j < state.CartList[i].prices.length; j++) {
+                                tempprice = 
+                                    tempprice +
+                                    parseFloat(state.CartList[i].prices[j].price) *
+                                        state.CartList[i].prices[j].quantity;
+                            }
+                            state.CartList[i].ItemPrice = tempprice.toFixed(2).toString()
+                            totalprice = totalprice + tempprice;
+                        }
+                        state.CartPrice = totalprice.toFixed(2).toString();
+                    }),
+                ),
+
+            addToFavoriteList: (type: string, id: string) =>
+                set(
+                    produce(state => {
+                        if (type == 'Coffee') {
+                            for (let i = 0; i < state.CoffeeList.length; i++) {
+                                if (state.CoffeeList[i].id == id) {
+                                    if (state.CoffeeList[i].favourite == false) {
+                                        state.CoffeeList[i].favourite = true;
+                                        state.FavoritesList.unshift(state.CoffeeList[i]);
+                                    }
+                                    break;
+                                }
+                            }
+                        }else if (type == 'Bean') {
+                            for (let i = 0; i < state.BeanList.length; i++) {
+                                if (state.BeanList[i].id == id) {
+                                    if (state.BeanList[i].favourite == false) {
+                                        state.BeanList[i].favourite = true;
+                                        state.FavoritesList.unshift(state.BeanList[i]);
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                    }),
+                ),
+            
+            deleteFromFavoriteList: (type: string, id: string) => 
+                set(
+                    produce(state => {
+                        if (type == 'Coffee') {
+                            for (let i = 0; i < state.CoffeeList.length; i++) {
+                                if (state.CoffeeList[i].id == id) {
+                                    if (state.CoffeeList[i].favourite == true) {
+                                        state.CoffeeList[i].favourite = false;
+                                        state.FavoritesList.unshift(state.CoffeeList[i]);
+                                    }
+                                    break;
+                                }
+                            }
+                        } else if (type == 'Bean') {
+                            for (let i = 0; i < state.BeanList.length; i++) {
+                                if (state.BeanList[i].id == id) {
+                                    if (state.BeanList[i].favourite == true) {
+                                        state.BeanList[i].favourite = false;
+                                        state.FavoritesList.unshift(state.BeanList[i]);
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+
+                        let spliceIndex = -1;
+                        for (let i = 0; i < state.FavoritesList.length; i++) {
+                            if (state.FavoritesList[i].id == id) {
+                                spliceIndex = 1;
+                                break;
+                            }
+                        }
+                        state.FavoritesList.splice(spliceIndex, 1);
+                    }),
+                ),
+
         }),
+
         {
-            name: "coffe-app",
+            name: "coffee-app",
             storage: createJSONStorage(() => AsyncStorage),
         },
     ),
